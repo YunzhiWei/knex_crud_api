7bdcfaf630a071ee9483c719a4b3a9a9fd79dfa6
Chris Wei <yunzhi.wei@live.com>
2017-06-29 14:20:33 +0800

    modify knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,12 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      database: 'nodejsbookdb',
+      user:     'nodejs_app',
+      password: '123456'
     }
   }
-
 };
