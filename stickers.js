10496b6caabb821ba95e3ad01ce87494fa319512
Chris Wei <yunzhi.wei@live.com>
2017-06-29 14:45:56 +0800

    prepare data file and execute the following seed command > knex seed:run

---
+++ stickers.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    title: "Javascript",
+    description: "The frontend language.",
+    rating: 3,
+    url: "http://www.javascript.com"
+  },
+  {
+    title: "NodeJS",
+    description: "Use the JS in the server side.",
+    rating: 5,
+    url: "http://www.nodejs.org"
+  },
+  {
+    title: "ReactJS",
+    description: "Frontend framework from Facebook",
+    rating: 6,
+    url: "http://www.reactjs.com"
+  },
+  {
+    title: "Mobx",
+    description: "Solution for data store",
+    rating: 8,
+    url: "http://www.mobxjs.io"
+  },
+  {
+    title: "Express",
+    description: "Framework for web server",
+    rating: 4,
+    url: "http://www.express.io"
+  }
+];
