de6279d1ce75cf691f279b43c570a33491640488
Chris Wei <yunzhi.wei@live.com>
2017-07-29 17:19:32 +0800

    User supertest to test restful api

--- test/app.test.js
+++ test/app.test.js
@@ -1,19 +1,28 @@
+const request = require('supertest');
 const knex = require('../db/knex');
 
+const app = require('../app');
+
 describe('CRUD KNEX', () => {
-  console.log("testing!");
+  // console.log("testing!");
   before((done) => {
-    console.log("migration start!");
+    // console.log("migration start!");
     // run migrations
     knex.migrate.latest()
       .then(() => {
-        console.log("seed start!");
+        // console.log("seed start!");
         // run seeds
         return knex.seed.run();
       }).then(() => { done() } )
   });
 
-  it("works ...", function() {
-    console.log("Its working!");
+  it("Lists all records", (done) => {
+    // console.log("Its working!");
+
+    request(app)
+      .get('/api/v1/stickers')
+      .set('Accept', 'applicaion/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   })
 });
