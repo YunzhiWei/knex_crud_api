c0f3fa3a8f804cbc7a9c15651fb42e0de5202de5
Chris Wei <yunzhi.wei@live.com>
2017-07-29 20:10:42 +0800

    new router and validatation

--- api/stickers.js
+++ api/stickers.js
@@ -4,10 +4,25 @@ const router = express.Router();
 
 const queries = require('../db/queries');
 
+function isValidId(req, res, next) {
+  if(!isNaN(req.params.id)) return next();
+  nex(new Error('Invalid Id'));
+}
+
 router.get('/', (req, res) => {
   queries.getAll().then(stickers => {
     res.json(stickers);
   });
+});
+
+router.get('/:id', isValidId, (req, res) => {
+  res.json({
+    message: 'Hello!'
+  });
+  
+  // queries.getAll().then(stickers => {
+  //   res.json(stickers);
+  // });
 });
 
 module.exports = router;
