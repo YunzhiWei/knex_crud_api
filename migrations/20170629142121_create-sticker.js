b60b1298d443bc8a0685fac2d328d9584bc814c9
Chris Wei <yunzhi.wei@live.com>
2017-07-31 00:26:02 +0800

    more example code

--- migrations/20170629142121_create-sticker.js
+++ migrations/20170629142121_create-sticker.js
@@ -9,17 +9,36 @@ exports.up = function(knex, Promise) {
   });
 };
 
-// exports.up = function(knex, Promise) {
-//   return knex.schema.createTable('todo', (table) => {
-//     table.increments();
-//     table.text('title').notNullable();
-//     table.integer('proiority').notNullable();
-//     table.text('description');
-//     table.boolean('done').defaultTo(false).notNullable();
-//     table.datetime('date').defaultTo(new Date()).notNullable();
-//   });
-// };
-
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('sticker');
 };
+
+// exports.up = function(knex, Promise) {
+//   return knex.schema
+//      .createTable('users', (table) => {
+//        table.increments();
+//        table.text('name').notNullable();
+//        table.text('email').notNullable();
+//        table.timestamp('create_at').defaultTo(knex.fn.now());
+//        table.timestamp('update_at').defaultTo(knex.fn.now());
+//   })
+//      .createTable('todos', (table) => {
+//        table.increments();
+//        table.text('title').notNullable();
+//        table.integer('proiority').notNullable();
+//        table.text('description');
+//        table.boolean('done').defaultTo(false).notNullable();
+//        table.datetime('due_date').defaultTo(new Date()).notNullable();
+//        table.timestamp('create_at').defaultTo(knex.fn.now());
+//        table.timestamp('update_at').defaultTo(knex.fn.now());
+//        table.integer('user_id').references('id').inTable('users');
+//   })
+//;
+// };
+
+// exports.down = function(knex, Promise) {
+//   return knex.schema
+//     .dropTable('users')
+//     .dropTable('todos')
+//   ;
+// };
