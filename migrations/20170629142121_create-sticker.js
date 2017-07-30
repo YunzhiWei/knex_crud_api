f8d87846ac477f7363123afcd1fb45eecef2bdae
Chris Wei <yunzhi.wei@live.com>
2017-07-30 17:46:59 +0800

    more example code

--- migrations/20170629142121_create-sticker.js
+++ migrations/20170629142121_create-sticker.js
@@ -1,14 +1,24 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('sticker',
-    (table) => {
-      table.increments();
-      table.text('title');
-      table.text('description');
-      table.float('rating');
-      table.text('url');
-    });
+  return knex.schema.createTable('sticker', (table) => {
+    table.increments();
+    table.text('title');
+    table.text('description');
+    table.float('rating');
+    table.text('url');
+  });
 };
+
+// exports.up = function(knex, Promise) {
+//   return knex.schema.createTable('todo', (table) => {
+//     table.increments();
+//     table.text('title').notNullable();
+//     table.integer('proiority').notNullable();
+//     table.text('description');
+//     table.boolean('done').defaultTo(false).notNullable();
+//     table.datetime('date').defaultTo(new Date()).notNullable();
+//   });
+// };
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('sticker');
